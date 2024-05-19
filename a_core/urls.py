f1fc169a4eb2bc444720846c48520c4ee12d0fce
salahYTus <usmonovsalokhiddin@gmail.com>
2024-05-19 17:48:33 +0500

    prod

--- a_core/urls.py
+++ a_core/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
 
 from django.conf.urls.static import static
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
