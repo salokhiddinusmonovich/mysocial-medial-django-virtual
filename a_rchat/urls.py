7d8ad7d9dd26d9cb008a75d429bd593ca9609284
salahYTus <usmonovsalokhiddin@gmail.com>
2024-06-08 21:39:17 +0500

    first commit

--- a_rchat/urls.py
+++ a_rchat/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', chat_view, name="chatting"),
     path('', lambda request: redirect('chatroom', chatroom_name='OurChat'), name='home'),
 ]
