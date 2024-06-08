feed44806b4179a83f2d4417fec30dfa9eda51c5
salahYTus <usmonovsalokhiddin@gmail.com>
2024-06-08 21:59:10 +0500

    first commit

--- a_core/settings.py
+++ a_core/settings.py
@@ -103,10 +103,11 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
+            'hosts': [('redis', 6379)],  # Assuming the Redis service is named 'redis' in Docker Compose
         },
     },
 }
+
 
 
 
