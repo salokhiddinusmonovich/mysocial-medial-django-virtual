77529430928e93f1adb99995b7990639155bc8f7
salahYTus <usmonovsalokhiddin@gmail.com>
2024-06-11 11:30:37 +0500

    first commit

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -1,9 +1,8 @@
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
