d8719e09e91e54ea8e8c1c49190ac920384f075f
salahYTus <usmonovsalokhiddin@gmail.com>
2024-06-11 11:26:36 +0500

    first commit

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -1,19 +1,9 @@
-"""
-ASGI config for a_core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
@@ -23,7 +13,11 @@ from a_rchat import routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-    )
+    "websocket": AuthMiddlewareStack(
+        AllowedHostsOriginValidator(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    ),
 })
