0e640ffd729abd6eebee60ab86e23cb4dd6b36e4
Salokhiddin <usmonovsalokhiddin@gmail.com>
2025-08-02 06:33:34 +0500

    done!

--- a_posts/forms.py
+++ a_posts/forms.py
@@ -14,7 +14,7 @@ class PostCreatForm(ModelForm):
         }
         widgets = {
             'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption...', 'class': 'font1 text-4xl'}),
-            'url': forms.TextInput(attrs={'placeholder': 'Add url address pictures from flickr...'}),
+            'url': forms.TextInput(attrs={'placeholder': 'Add url address pictures from painterest...'}),
             'tags': forms.CheckboxSelectMultiple(),
         }
 
